76ffe8ee995446743735297700844e7c56658987
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-22 21:14:55 +0000

    Without parentheses, any code on the lines after return will be ignored!

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 
-export default function App() {
-  return (
+export default function App() {// Prefix lets you mark the main function in a file so that you can later import it from other files.
+  return (//return keyword, you must wrap it in a pair of parentheses:
     <main>
        <h1>Hello World!</h1>
       <img 
         src='https://picsum.photos/id/111/1100/900'
         alt='Must Be A Cadillac'/>
     </main>
-  )
+  );
 }
