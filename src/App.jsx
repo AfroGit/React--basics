d0606498e656eefa5dfb6132d787704d7829a891
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-22 22:01:41 +0000

    So much fun just adding all sorts of stuff on to the DOM.

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import './App.css'
 export default function App() {// Prefix lets you mark the main function in a file so that you can later import it from other files.
   return (//return keyword, you must wrap it in a pair of parentheses:
     <main>
-       <h1>Hello World!</h1>
+       <h1>CARS WE LOVE!</h1>
       <img 
         src='https://picsum.photos/id/111/1100/900'
         alt='Must Be A Cadillac'/>
+      <p>It was brash and it was loud – the 1940s put paid to the glumness of the ’30s and nowhere was that more obvious than in the cars we drove, which took a quantum leap in durability, performance, equipment and style.</p>
+      <section>
+        <img src='https://picsum.photos/id/133/200/300' alt='Age Aint NOthin But A Number'/>
+      </section>
     </main>
   );
 }
