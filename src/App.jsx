c55b65c050ca69d9c6c3630687f9a3b3e425d703
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-22 21:07:02 +0000

    React components are regular JavaScript functions, but their names must start with a capital letter or they won’t work!

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ export default function App() {
   return (
     <main>
        <h1>Hello World!</h1>
+      <img 
+        src='https://picsum.photos/id/111/1100/900'
+        alt='Must Be A Cadillac'/>
     </main>
   )
 }
