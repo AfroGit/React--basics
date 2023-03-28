7409e4a6533b752472776d72af2550e227b69a85
Maurice Mutua <94635472+AfroGit@users.noreply.github.com>
2023-03-28 18:33:25 +0000

    Created my first button on React and used it in an app. Ln 18

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import './App.css'
 
+function MyButton(){
+  return(
+    <button>I'M A Button</button>
+  );
+}
+
 export default function App() {// Prefix lets you mark the main function in a file so that you can later import it from other files.
   return (//return keyword, you must wrap it in a pair of parentheses:
     <main>
@@ -7,10 +13,15 @@ export default function App() {// Prefix lets you mark the main function in a fi
       <img 
         src='https://picsum.photos/id/111/1100/900'
         alt='Must Be A Cadillac'/>
-      <p>It was brash and it was loud – the 1940s put paid to the glumness of the ’30s and nowhere was that more obvious than in the cars we drove, which took a quantum leap in durability, performance, equipment and style.</p>
+      
+      <article>
+      <MyButton />
+        </article>
+      
       <section>
         <img src='https://picsum.photos/id/133/200/300' alt='Age Aint NOthin But A Number'/>
       </section>
+    
     </main>
   );
 }
